cae0574d3a95584f15ff0a7f1c752644a4b7ea3e
yogee-chintu <yogeeshwarm@brightcomgroup.com>
2023-03-01 12:32:56 +0000

    100-createIteratorObject.js

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,13 @@
 export default function createIteratorObject(report) {
-  return
+
+  let employees = [];
+
+  for (const item of Object.values(report.allEmployees)) {
+
+    employees = [...employees, ...item];
+
+  }
+
+  return employees;
+
 }
